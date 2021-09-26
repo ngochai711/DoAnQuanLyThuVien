93c7f9a90b64336ed6de49e769a6fa0df23f4006
lehquy13 <lehquy13@gmail.com>
2021-09-26 20:17:57 +0700

    Quý thay đổi giao diện chính

--- DoAnQuanLyThuVien/fMain.Designer.cs
+++ DoAnQuanLyThuVien/fMain.Designer.cs
@@ -29,112 +29,157 @@ namespace DoAnQuanLyThuVien
         /// </summary>
         private void InitializeComponent()
         {
-            this.SlidingPanel = new System.Windows.Forms.Panel();
-            this.btnShow = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
-            this.btnAssist = new System.Windows.Forms.Button();
-            this.btnL_card_show = new System.Windows.Forms.Button();
-            this.bntAcountInfo = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
             this.btnBookManagement = new System.Windows.Forms.Button();
+            this.bntAcountInfo = new System.Windows.Forms.Button();
+            this.btnL_card_show = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnAssist = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lbTeamName = new System.Windows.Forms.Label();
+            this.SlidingPanel = new System.Windows.Forms.Panel();
             this.SlidingPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // SlidingPanel
+            // btnBookManagement
             // 
-            this.SlidingPanel.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.SlidingPanel.Controls.Add(this.btnShow);
-            this.SlidingPanel.Controls.Add(this.btnExit);
-            this.SlidingPanel.Controls.Add(this.btnAssist);
-            this.SlidingPanel.Controls.Add(this.btnL_card_show);
-            this.SlidingPanel.Controls.Add(this.bntAcountInfo);
-            this.SlidingPanel.Controls.Add(this.btnBookManagement);
-            this.SlidingPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.SlidingPanel.Location = new System.Drawing.Point(0, 0);
-            this.SlidingPanel.Name = "SlidingPanel";
-            this.SlidingPanel.Size = new System.Drawing.Size(304, 653);
-            this.SlidingPanel.TabIndex = 0;
+            this.btnBookManagement.BackColor = System.Drawing.Color.White;
+            this.btnBookManagement.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnBookManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBookManagement.Image = ((System.Drawing.Image)(resources.GetObject("btnBookManagement.Image")));
+            this.btnBookManagement.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBookManagement.Location = new System.Drawing.Point(3, 85);
+            this.btnBookManagement.Name = "btnBookManagement";
+            this.btnBookManagement.Size = new System.Drawing.Size(312, 70);
+            this.btnBookManagement.TabIndex = 0;
+            this.btnBookManagement.Text = "Quản Lý Sách";
+            this.btnBookManagement.UseVisualStyleBackColor = false;
             // 
-            // btnShow
+            // bntAcountInfo
             // 
-            this.btnShow.Location = new System.Drawing.Point(0, 0);
-            this.btnShow.Name = "btnShow";
-            this.btnShow.Size = new System.Drawing.Size(41, 36);
-            this.btnShow.TabIndex = 5;
-            this.btnShow.Text = "Hien";
-            this.btnShow.UseVisualStyleBackColor = true;
-            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
-            // 
-            // btnExit
-            // 
-            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnExit.Location = new System.Drawing.Point(48, 500);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(209, 70);
-            this.btnExit.TabIndex = 4;
-            this.btnExit.Text = "Thoát";
-            this.btnExit.UseVisualStyleBackColor = true;
-            // 
-            // btnAssist
-            // 
-            this.btnAssist.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnAssist.Location = new System.Drawing.Point(48, 400);
-            this.btnAssist.Name = "btnAssist";
-            this.btnAssist.Size = new System.Drawing.Size(209, 70);
-            this.btnAssist.TabIndex = 3;
-            this.btnAssist.Text = "Hỗ Trợ";
-            this.btnAssist.UseVisualStyleBackColor = true;
+            this.bntAcountInfo.FlatAppearance.BorderSize = 0;
+            this.bntAcountInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bntAcountInfo.Image = ((System.Drawing.Image)(resources.GetObject("bntAcountInfo.Image")));
+            this.bntAcountInfo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bntAcountInfo.Location = new System.Drawing.Point(3, 178);
+            this.bntAcountInfo.Name = "bntAcountInfo";
+            this.bntAcountInfo.Size = new System.Drawing.Size(312, 70);
+            this.bntAcountInfo.TabIndex = 1;
+            this.bntAcountInfo.Text = "Thông Tin Tài Khoản";
+            this.bntAcountInfo.UseVisualStyleBackColor = true;
+            this.bntAcountInfo.Click += new System.EventHandler(this.bntAcountInfo_Click);
             // 
             // btnL_card_show
             // 
             this.btnL_card_show.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnL_card_show.Location = new System.Drawing.Point(48, 300);
+            this.btnL_card_show.FlatAppearance.BorderSize = 0;
+            this.btnL_card_show.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnL_card_show.Image = ((System.Drawing.Image)(resources.GetObject("btnL_card_show.Image")));
+            this.btnL_card_show.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnL_card_show.Location = new System.Drawing.Point(3, 271);
             this.btnL_card_show.Name = "btnL_card_show";
-            this.btnL_card_show.Size = new System.Drawing.Size(209, 70);
+            this.btnL_card_show.Size = new System.Drawing.Size(312, 70);
             this.btnL_card_show.TabIndex = 2;
             this.btnL_card_show.Text = "Mượn Trả Sách";
             this.btnL_card_show.UseVisualStyleBackColor = true;
+            this.btnL_card_show.Click += new System.EventHandler(this.btnL_card_show_Click);
             // 
-            // bntAcountInfo
+            // btnShow
             // 
-            this.bntAcountInfo.Location = new System.Drawing.Point(48, 200);
-            this.bntAcountInfo.Name = "bntAcountInfo";
-            this.bntAcountInfo.Size = new System.Drawing.Size(209, 70);
-            this.bntAcountInfo.TabIndex = 1;
-            this.bntAcountInfo.Text = "Thông Tin Tài Khoản";
-            this.bntAcountInfo.UseVisualStyleBackColor = true;
+            this.btnShow.BackColor = System.Drawing.Color.White;
+            this.btnShow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnShow.Image = ((System.Drawing.Image)(resources.GetObject("btnShow.Image")));
+            this.btnShow.Location = new System.Drawing.Point(0, 0);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(50, 48);
+            this.btnShow.TabIndex = 5;
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
             // 
-            // btnBookManagement
+            // btnAssist
             // 
-            this.btnBookManagement.Location = new System.Drawing.Point(48, 100);
-            this.btnBookManagement.Name = "btnBookManagement";
-            this.btnBookManagement.Size = new System.Drawing.Size(209, 70);
-            this.btnBookManagement.TabIndex = 0;
-            this.btnBookManagement.Text = "Quản Lý Sách";
-            this.btnBookManagement.UseVisualStyleBackColor = true;
+            this.btnAssist.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAssist.FlatAppearance.BorderSize = 0;
+            this.btnAssist.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAssist.Image = ((System.Drawing.Image)(resources.GetObject("btnAssist.Image")));
+            this.btnAssist.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAssist.Location = new System.Drawing.Point(0, 364);
+            this.btnAssist.Name = "btnAssist";
+            this.btnAssist.Size = new System.Drawing.Size(315, 70);
+            this.btnAssist.TabIndex = 8;
+            this.btnAssist.Text = "Hỗ Trợ";
+            this.btnAssist.UseVisualStyleBackColor = true;
+            this.btnAssist.Click += new System.EventHandler(this.btnAssist_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(3, 457);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(312, 70);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lbTeamName
+            // 
+            this.lbTeamName.AutoSize = true;
+            this.lbTeamName.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTeamName.Location = new System.Drawing.Point(56, 0);
+            this.lbTeamName.Name = "lbTeamName";
+            this.lbTeamName.Size = new System.Drawing.Size(247, 56);
+            this.lbTeamName.TabIndex = 10;
+            this.lbTeamName.Text = "AppName";
+            // 
+            // SlidingPanel
+            // 
+            this.SlidingPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.SlidingPanel.BackColor = System.Drawing.Color.White;
+            this.SlidingPanel.Controls.Add(this.lbTeamName);
+            this.SlidingPanel.Controls.Add(this.btnExit);
+            this.SlidingPanel.Controls.Add(this.btnAssist);
+            this.SlidingPanel.Controls.Add(this.btnShow);
+            this.SlidingPanel.Controls.Add(this.btnL_card_show);
+            this.SlidingPanel.Controls.Add(this.bntAcountInfo);
+            this.SlidingPanel.Controls.Add(this.btnBookManagement);
+            this.SlidingPanel.Location = new System.Drawing.Point(1, 1);
+            this.SlidingPanel.Name = "SlidingPanel";
+            this.SlidingPanel.Size = new System.Drawing.Size(318, 674);
+            this.SlidingPanel.TabIndex = 0;
+            this.SlidingPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.SlidingPanel_Paint);
             // 
             // fMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 653);
+            this.ClientSize = new System.Drawing.Size(1124, 672);
             this.Controls.Add(this.SlidingPanel);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "fMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản Lý Thư Viện";
+            this.Load += new System.EventHandler(this.fMain_Load);
             this.SlidingPanel.ResumeLayout(false);
+            this.SlidingPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel SlidingPanel;
-        private System.Windows.Forms.Button btnShow;
-        private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Button btnAssist;
-        private System.Windows.Forms.Button btnL_card_show;
-        private System.Windows.Forms.Button bntAcountInfo;
         private System.Windows.Forms.Button btnBookManagement;
+        private System.Windows.Forms.Button bntAcountInfo;
+        private System.Windows.Forms.Button btnL_card_show;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnAssist;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbTeamName;
+        private System.Windows.Forms.Panel SlidingPanel;
     }
 }
--- DoAnQuanLyThuVien/fMain.cs
+++ DoAnQuanLyThuVien/fMain.cs
@@ -12,25 +12,68 @@ namespace DoAnQuanLyThuVien
 {
     public partial class fMain : Form
     {
-        int panelWidth;
+        int oldPanelWidth,oldPanelHeight;
+
         bool hidden;
         public fMain()
         {
             InitializeComponent();
-            panelWidth = SlidingPanel.Width;
+            oldPanelWidth = SlidingPanel.Width;
+            oldPanelHeight = SlidingPanel.Height;
             hidden = false;
+
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAssist_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnL_card_show_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntAcountInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SlidingPanel_Paint(object sender, PaintEventArgs e)
+        {
+
         }
 
         private void btnShow_Click(object sender, EventArgs e)
         {
+            if(SlidingPanel.Width > btnShow.Width)
+            {
+                oldPanelWidth = SlidingPanel.Width;
+                oldPanelHeight = SlidingPanel.Height;
+            }
+            
             if (!hidden)
             {
-                SlidingPanel.Width = 41;
+                SlidingPanel.Width = btnShow.Width;
+                SlidingPanel.Height = btnShow.Height;
                 hidden = true;
             }
             else
             {
-                SlidingPanel.Width = 304;
+               
+              
+                SlidingPanel.Width = oldPanelWidth;
+                SlidingPanel.Height = oldPanelHeight;
                 hidden = false;
             }
                 
